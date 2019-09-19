4b720ff503530413144760b48d3266739ad1435f
Alex Glau <glaualex89@gmail.com>
2019-09-19 11:22:07 +0500

    Deleted api key

--- mybot/bot.py
+++ mybot/bot.py
@@ -4,7 +4,7 @@ PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 def main():
-    mybot = Updater('854780013:AAFlSL13uIjGvutGeoKA4XpIlpy53dL4_nY', request_kwargs=PROXY)
+    mybot = Updater('', request_kwargs=PROXY)
     mybot.start_polling()
     mybot.idle()
 
