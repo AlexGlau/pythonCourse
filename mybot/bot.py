33da8098672ec9d32c48fd75f0081047f07c09a2
Alex Glau <glaualex89@gmail.com>
2019-09-21 16:03:09 +0500

    Fixed bot starting + some functions

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,10 +1,30 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+PROXY = {'proxy_url': 'socks5h://t2.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 def main():
     mybot = Updater('854780013:AAFlSL13uIjGvutGeoKA4XpIlpy53dL4_nY', request_kwargs=PROXY)
+
+    def greet_user(bot, update):
+        text = '/start called'
+        update.message.reply_text(text)
+
+    def talk_to_me(bot, update):
+        user_text = update.message.text
+        print(f'user msg: {user_text}')
+        update.message.reply_text(user_text)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
     mybot.start_polling()
     mybot.idle()
 
