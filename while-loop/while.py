294d1c9d751e6d00b3a9abdc0d6e9e817851a33d
Alex Glau <glaualex89@gmail.com>
2019-09-23 09:06:18 +0500

    While loop

---
+++ while-loop/while.py
@@ -0,0 +1,20 @@
+answers = {
+    'How are you?': 'Good',
+    'What are you doing?': 'I am coding',
+    'Do you want to eat?': 'No, thanks!',
+    'Do you know where is your wife now?': 'What?!!'
+}
+
+def ask_user():
+    while True:
+        user_say = input('Enter your message:\n')
+        if user_say in answers:
+            program_answer = answers[user_say]
+            print(f'- Program: {program_answer}')
+        elif (user_say == 'Thanks, that`s all!'):
+            print('Finally!')
+            break
+        else:
+            print('Incorrect message.')
+
+ask_user()
